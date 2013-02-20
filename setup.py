649b1d6c2eccf6c5c219767a5b74bee923e7206c
Mathias Loesch <mathias.loesch@uni-bielefeld.de>
2013-02-20 14:12:31 +0100

    Don't import __version__

--- setup.py
+++ setup.py
@@ -11,12 +11,14 @@
 
 from setuptools import setup, find_packages
 
-from sickle import __version__ as version
 
 setup(
     name='sickle',
-    version=version,
+    version='0.1',
     long_description=__doc__,
+    install_requires=[
+        'requests>=1.1.0',
+    ],
     packages=find_packages(),
     zip_safe=False,
 )
