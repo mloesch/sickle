8052aa5f1f97d36d607e187afc3181d837966f26
Mathias Loesch <mathias.loesch@uni-bielefeld.de>
2013-02-26 11:00:57 +0100

    Defined OAI-specific exceptions

---
+++ sickle/oaiexceptions.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+"""
+    exceptions
+    ~~~~~~~~~~
+
+    OAI errors.
+
+    :copyright: Copright 2013 Mathias Loesch
+"""
+
+
+class BadArgument(Exception):
+    pass
+
+class BadVerb(Exception):
+    pass
+
+class BadResumptionToken(Exception):
+    pass
+
+class CannotDisseminateFormat(Exception):
+    pass
+
+class IdDoesNotExist(Exception):
+    pass
+
+class NoSetHierarchy(Exception):
+    pass
+
+class NoMetadataFormat(Exception):
+    pass
+
+class OAIError(Exception):
+    pass
