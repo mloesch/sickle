a3bcfac2753fc68b3317b3a249be1b8bce15220e
Mathias Loesch <m.loesch83@gmail.com>
2015-11-15 10:02:48 +0100

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sickle'
-copyright = u'2013, Mathias Loesch'
+copyright = u'2015, Mathias Loesch'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
